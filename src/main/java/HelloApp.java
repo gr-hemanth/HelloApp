e49e75289c640c8dd4a04504695b98c1d9d2ac6a
Hemanth <grhemanth123@gmail.com>
2026-03-11 15:47:33 +0530

    Coded UC1:Display hello world in console

---
+++ src/main/java/HelloApp.java
@@ -0,0 +1,21 @@
+/**
+ * HelloApp.java
+ * A basic Java program that prints "Hello, World!" to the console.
+ *
+ * This program demonstrates the structure of a simple Java application,
+ * including class declaration, the main method, and console output.
+ *
+ * @author Hemanth
+ * @version 1.0
+ */
+
+public class HelloApp {
+
+    /**
+     * Main method – entry point of the Java program.
+     * It prints a greeting message to the console.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
